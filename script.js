85c5cbe1c14d878aa21367ebd350a07373ebfe85
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-03 18:09:31 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// ==========================
+// RAXY BOOSTER
+// Main JavaScript
+// ==========================
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🚀 RAXY BOOSTER Loaded Successfully");
+});
