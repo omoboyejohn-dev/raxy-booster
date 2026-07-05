0683e97553648769df26ba4602aca8e08449c077
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-05 20:40:04 +0100

    Update admin-guard.js

--- admin-guard.js
+++ admin-guard.js
@@ -1,1 +1,15 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
+const ADMIN_EMAIL = "raxysocial365@gmail.com";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  } else {
+    if (user.email !== ADMIN_EMAIL) {
+      alert("Access denied: Admin only");
+      window.location.href = "dashboard.html";
+    }
+  }
+});
