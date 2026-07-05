c668e9faa618b82a2aabde6a27231088b74658d6
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-05 11:08:58 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,29 @@
+import { auth } from "./firebase.js";
 
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Login successful!");
+
+        window.location.href = "services.html";
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
