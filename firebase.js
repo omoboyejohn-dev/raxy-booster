f154c7e3f03ad71a7b3d9f1fc8d0747e21a06a13
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-05 19:18:59 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,20 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
-// Firebase configuration
+// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyC_-MVpov7-MZKxGegsqvrbB4_a3a0WGHM",
-  authDomain: "raxy-booster.firebaseapp.com",
-  projectId: "raxy-booster",
-  storageBucket: "raxy-booster.firebasestorage.app",
-  messagingSenderId: "629973087965",
-  appId: "1:629973087965:web:f274a36384968b814c0d34",
-  measurementId: "G-L520MSR5L5"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Firebase services
-const auth = getAuth(app);
-const db = getFirestore(app);
-
 // Export services
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
